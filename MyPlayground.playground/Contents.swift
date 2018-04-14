c566b028014dcd02660e2cf4003fef89bc2ef17e
Briant <briantfabela@gmail.com>
2018-04-14 16:59:13 -0700

    added random func for Int's
    ...

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,12 +2,15 @@
 
 import UIKit
 
+// return number between 'start' and (including) 'end' with 'decimals' amount of decimals
 func randomInRangeDouble(start: Int, end: Int, decimals: Double) -> Double {
+    // constants made outside return statement to prevent arithmetic from taking place as parameter
     let n: Double = Double(arc4random_uniform(UInt32(end - start))) + drand48() + Double(start)
-    let nPlaces: Double = pow(10, decimals)
+    let nPlaces: Double = pow(10, decimals) // 'n' rounded to 'nPlaces'
     return Double(round(nPlaces * n)/nPlaces)
 }
 
-for _ in 1...20 {
-    print(randomInRangeDouble(start: 0, end: 5, decimals: 2))
+func randomInRangeInt (start: Int, end: Int) -> Int {
+    let n: Int = (end - start) + 1 // prevent arithmetic from taking place as parameter
+    return Int(arc4random_uniform(UInt32(n))) + start
 }
